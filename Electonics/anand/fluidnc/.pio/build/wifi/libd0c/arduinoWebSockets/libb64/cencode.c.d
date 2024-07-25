ebfe2c0f8ff266ec2a45df6ea8353519bbb881b2
Anand k vinu <adnokiia@gmail.com>
2024-07-25 18:03:36 +0530

    added penmodule m code and tested

--- Electonics/anand/fluidnc/.pio/build/wifi/libd0c/arduinoWebSockets/libb64/cencode.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/wifi/libd0c/arduinoWebSockets/libb64/cencode.c.o: \
- .pio/libdeps/wifi/arduinoWebSockets/src/libb64/cencode.c
--- Electonics/anand/fluidnc/.pio/build/wifi/libd0c/arduinoWebSockets/libsha1/libsha1.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/wifi/libd0c/arduinoWebSockets/libsha1/libsha1.c.o: \
- .pio/libdeps/wifi/arduinoWebSockets/src/libsha1/libsha1.c
